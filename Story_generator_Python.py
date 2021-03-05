277eb01eaad3a6b583262229a0d9eb735b96f3b2
sabina <aktersabina122@gmail.com>
2021-03-04 20:26:12 -0500

    removing quation

--- Story_generator_Python.py
+++ Story_generator_Python.py
@@ -1,5 +1,5 @@
 
-'''
+
 # Challenge One (Input and String Formatting)
 # You and a partner will try to use your knowledge of Python to create a MadLib function.
 
@@ -9,7 +9,7 @@
 
 # TODO: Create a Python program that swaps those items in your story
 
-# TODO: Print the user's story to the command line'''
+# TODO: Print the user's story to the command line
 
 #====================================================================================================
 
